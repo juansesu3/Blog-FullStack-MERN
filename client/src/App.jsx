038d004828746015b68c3db6fe804dbbffa80572
juansesu3 <juanseu3@hotmail.com>
2023-06-05 14:27:20 +0200

    working on html structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,59 @@
-
-
+import "./App.css";
 
 function App() {
-  
   return (
     <>
-    <div>Test</div>
+      <main>
+        <header>
+          <a href="" className="logo">
+            My logo
+          </a>
+          <nav>
+            <a href="" className="">
+              Login
+            </a>
+            <a href="" className="">
+              Register
+            </a>
+          </nav>
+        </header>
+
+        <div className="post">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=730&crop=1" />
+          <h2>
+            EcoFlow teases full-house battery backup coming later this year
+          </h2>
+          <p>
+            Today at its special launch event, home backup power giant EcoFlow
+            launched a flurry of new products, including a “Whole-Home Backup
+            Power Solution.”
+          </p>
+        </div>
+        <div className="post">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=730&crop=1" />
+          <h2>
+            EcoFlow teases full-house battery backup coming later this year
+          </h2>
+          <p>
+            Today at its special launch event, home backup power giant EcoFlow
+            launched a flurry of new products, including a “Whole-Home Backup
+            Power Solution.”
+          </p>
+        </div>
+        <div className="post">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=730&crop=1" />
+          <h2>
+            EcoFlow teases full-house battery backup coming later this year
+          </h2>
+          <p>
+            Today at its special launch event, home backup power giant EcoFlow
+            launched a flurry of new products, including a “Whole-Home Backup
+            Power Solution.”
+          </p>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
