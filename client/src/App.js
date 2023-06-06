5af52b45385d80bb800194e6b32193aa29853f91
juansesu3 <juanseu3@hotmail.com>
2023-06-06 09:38:45 +0200

    start

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">My logo</a>
+        <nav>
+          <a href="" className="">Login</a>
+          <a href="" className="">Register</a>
+        </nav>
+      </header>
+    </main>
+  )
+};
+
+export default App;
