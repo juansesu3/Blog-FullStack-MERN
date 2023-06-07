d01a41b6970333b1cf0c78447285afdd93a6b655
juansesu3 <juanseu3@hotmail.com>
2023-06-07 17:27:35 +0200

    I was finished with single page post now I still working on edit page

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { format } from "date-fns";
 
 const PostPage = () => {
   const [postInfo, setPostInfo] = useState(null);
@@ -17,11 +18,15 @@ const PostPage = () => {
 
   return (
     <div className="post-page">
+      <h1>{postInfo.title}</h1>
+      <time>{format(new Date(postInfo.createdAt), "MMM d yyyy HH:mm")}</time>
+      <div className="author">by @{postInfo.author.userName}</div>
       <div className="image">
         <img src={`http://localhost:7000/${postInfo.cover}`} />
       </div>
-      <h1>{postInfo.title}</h1>
-      <div dangerouslySetInnerHTML={{__html:postInfo.content}}></div>
+
+      
+      <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}}></div>
     </div>
   );
 };
