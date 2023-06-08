70fce87cc4c474a95f2a4c1caf06d2555a786f5c
juansesu3 <juanseu3@hotmail.com>
2023-06-08 11:22:01 +0200

    finished with edit post feature

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Registerpage from "./pages/RegisterPage";
 import { UserContextProvider } from "./userContext";
 import CreatePost from "./pages/CreatePost";
 import PostPage from "./pages/PostPage";
+import EditPost from "./pages/EditPost";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
           <Route path="/register" element={<Registerpage />} />
           <Route path="/create" element={<CreatePost />} />
           <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/edit/:id" element={<EditPost />} />
         </Route>
       </Routes>
     </UserContextProvider>
--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,40 +1,55 @@
-import { useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
 import Editor from "../components/Editor";
 
-
-const CreatePost = () => {
+const EditPost = () => {
+  const { id } = useParams();
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
   const [files, setFiles] = useState("");
+  const [cover, setCover] = useState("");
   const [redirect, setRedirect] = useState(false);
 
-  const createNewPost = async (ev) => {
+  useEffect(() => {
+    fetch("http://localhost:7000/post/" + id).then((response) => {
+      response.json().then((postInfo) => {
+        setTitle(postInfo.title);
+        setContent(postInfo.content);
+        setSummary(postInfo.summary);
+      });
+    });
+  }, []);
+
+  const updatedPost = async (ev) => {
+    ev.preventDefault();
+
     const data = new FormData();
+
     data.set("title", title);
     data.set("summary", summary);
     data.set("content", content);
-    data.set("file", files[0]);
-    ev.preventDefault();
+    data.set("id", id);
+    if (files?.[0]) {
+      data.set("file", files?.[0]);
+    }
+
     const response = await fetch("http://localhost:7000/post", {
-      method: "POST",
+      method: "PUT",
       body: data,
-      credentials:'include',
-      
+      credentials: "include",
     });
     if (response.ok) {
       setRedirect(true);
     }
   };
+
   if (redirect) {
-    return <Navigate to={"/"} />;
+    return <Navigate to={"/post/" + id} />;
   }
 
   return (
-    <form onSubmit={createNewPost}>
+    <form onSubmit={updatedPost}>
       <input
         type="text"
         placeholder="title"
@@ -52,10 +67,10 @@ const CreatePost = () => {
         }}
       />
       <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
-    <Editor onChange={setContent} value={content}/>
-      <button style={{ marginTop: "5px" }}>Create post</button>
+      <Editor onChange={setContent} value={content} />
+      <button style={{ marginTop: "5px" }}>Update post</button>
     </form>
   );
 };
 
-export default CreatePost;
+export default EditPost;
